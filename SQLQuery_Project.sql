79b8d77a390efa97148336656b9d798006a4804d
katrina864 <60376415+katrina864@users.noreply.github.com>
2022-03-30 07:55:24 -0500

    Add files via upload

--- SQLQuery_Project.sql
+++ SQLQuery_Project.sql
@@ -1,3 +1,40 @@
+-- Queries that output excel tables used in Tableau visualization
+
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+-- 2. 
+
+-- We take these out as they are not included in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by location, population
+order by PercentPopulationInfected desc
+
+-- 4.
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+-- Beginning of the rest of my queries
+
 SELECT *
 FROM PortfolioProject..CovidDeaths
 WHERE continent is not null
